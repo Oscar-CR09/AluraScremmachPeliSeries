4cdcc5687343bfe7b0ef8edc7f025a3b4131afdc
Oscar <oscarcr189@outlook.com>
2024-05-24 21:37:52 -0600

    series de navegador

--- src/main/java/com/Alura/ScremmPeliculas/controler/SerieControle.java
+++ src/main/java/com/Alura/ScremmPeliculas/controler/SerieControle.java
@@ -1,13 +1,21 @@
 package com.Alura.ScremmPeliculas.controler;
 
+import com.Alura.ScremmPeliculas.model.Serie;
+import com.Alura.ScremmPeliculas.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SerieControle {
+
+    @Autowired
+    private SerieRepository repository;
     @GetMapping("/series")
-    public String mostrarMensaje(){
-        return "Este es mi primer mensaje en mi appWeb";
+    public List<Serie> obtenerTodasLasSeries(){
+        return repository.findAll();
     }
 
 }
